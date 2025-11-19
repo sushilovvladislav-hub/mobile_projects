0587b37161654e8755dd3fd8670045d15e8030f5
unknown <sushilovvladislav@gmail.com>
2025-11-19 01:23:16 -0800

    Added new project with draft interface

---
+++ Prac_03_Fruits/app/src/main/java/koeait/group245/sushilov/prac_03_fruits/MainActivity.java
@@ -0,0 +1,24 @@
+package koeait.group245.sushilov.prac_03_fruits;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
